824f63a20a7c0094df1ae9eac219b5df015bb060
Haibo <haibo@users.noreply.github.com>
2021-06-21 18:44:20 +0100

    Chefs + didacticiel UI

--- Assets/Scripts/DidacticielScript.cs
+++ Assets/Scripts/DidacticielScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DidacticielScript : MonoBehaviour
 {
@@ -12,6 +13,13 @@ public class DidacticielScript : MonoBehaviour
 
     public Transform spawnSpot;
 
+    public TextMeshProUGUI text0;
+    public TextMeshProUGUI text1;
+    public TextMeshProUGUI text2;
+    public TextMeshProUGUI text3;
+    public TextMeshProUGUI text4;
+    public TextMeshProUGUI text5;
+
     public int didacticielStep;
 
     bool startDidacticiel;
@@ -19,6 +27,12 @@ public class DidacticielScript : MonoBehaviour
     void Start()
     {
         startDidacticiel = true;
+        text0.enabled = false;
+        text1.enabled = false;
+        text2.enabled = false;
+        text3.enabled = false;
+        text4.enabled = false;
+        text5.enabled = false;
     }
 
     void Update()
@@ -29,9 +43,12 @@ public class DidacticielScript : MonoBehaviour
             {
                 //montrer comment faire
 
-                if(fusedPlayer.GetComponent<Players>().merged == true)
+                text0.enabled = true;
+
+                if (fusedPlayer.GetComponent<Players>().merged == true)
                 {
                     didacticielStep++;
+                    text0.enabled = false;
                 }
 
 
@@ -40,9 +57,12 @@ public class DidacticielScript : MonoBehaviour
             {
                 //expliquer comment faire
 
-                if(fusedPlayer.GetComponent<Players>().splitting == true)
+                text1.enabled = true;
+
+                if (fusedPlayer.GetComponent<Players>().splitting == true)
                 {
                     didacticielStep++;
+                    text1.enabled = false;
                 }
 
             }
@@ -50,8 +70,12 @@ public class DidacticielScript : MonoBehaviour
             {
                 //demander de refusionner
 
+                text2.enabled = true;
+
                 if (fusedPlayer.GetComponent<Players>().merged == true)
                 {
+
+                    text2.enabled = false;
                     didacticielStep++;
                 }
 
@@ -59,9 +83,11 @@ public class DidacticielScript : MonoBehaviour
             else if (didacticielStep == 3)
             {
                 //défusionner + joystick
+                text3.enabled = true;
 
                 if (fusedPlayer.GetComponent<Players>().splitting == true && fusedPlayer.GetComponent<Players>().move != Vector3.zero)
                 {
+                    text3.enabled = false;
                     didacticielStep++;
                 }
 
@@ -69,9 +95,13 @@ public class DidacticielScript : MonoBehaviour
             else if (didacticielStep == 4)
             {
                 //casser barricade
+
+                text4.enabled = true;
+
                 GameObject barricade = GameObject.FindGameObjectWithTag("Breakable_Barricade");
                 if (barricade == null)
                 {
+                    text4.enabled = false;
                     didacticielStep++;
                 }
                 
@@ -80,11 +110,14 @@ public class DidacticielScript : MonoBehaviour
             {
                 //apparition chef + message fuite
 
+                text5.enabled = true;
+
                 GameObject enemy = Instantiate(chefPrefab, spawnSpot.position, spawnSpot.rotation);
 
 
                 enemy.GetComponent<EnemyScript>().Spot(1, player2.transform);
                 enemy.GetComponent<EnemyScript>().Spot(0, player1.transform);
+                enemy.GetComponent<EnemyScript>().spotted = true;
 
                 didacticielStep++;
             }
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -59,7 +59,7 @@ public class EnemyScript : MonoBehaviour
     public int currentJourneyPoint = 0;
 
     bool running;
-    public bool spotted;
+    public bool spotted = false;
     public bool work;
     bool lookingStraight;
     bool jumping;
@@ -76,7 +76,6 @@ public class EnemyScript : MonoBehaviour
 
         gravity = Vector3.down * gravityValue;
 
-        spotted = false;
 
         groundDetector = this.gameObject.transform.GetChild(1);
 
@@ -188,80 +187,9 @@ public class EnemyScript : MonoBehaviour
                 }
             }
 
-            /*
-            if (lookingStraight == true)
-            {
-                
-            }
-            else
-            {
-                //transform.eulerAngles = transform.eulerAngles + new Vector3(0, rotationSpeed * Time.deltaTime, 0);
-            }
-            */
-        }
-
-        /*if (distanceFromTarget <= chaseDistance)
-        
-            */
-
-        /*
-
-        if (target != null)
-        {
-            heightValue = target.transform.parent.transform.position.y - transform.position.y;
-        }
-        else
-        {
-            heightValue = 0;
-        }
-
-
-        for (int i = -5; i < 6; i++)
-        {
             
-            checkVector = Vector3.forward + new Vector3(0.2f * i, heightValue, 0);
-            adaptedChaseDistance = chaseDistance * (1 - Mathf.Abs(i * 0.125f));
-            distance = checkVector.magnitude;
-            normalizedVector = checkVector / distance;
-
-            //Debug.Log(normalizedVector);
-
-            Debug.DrawRay(transform.position, transform.TransformDirection(normalizedVector) * adaptedChaseDistance, Color.yellow);
-
-            RaycastHit hitTarget;
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(normalizedVector), out hitTarget, adaptedChaseDistance))
-            {
-                if (hitTarget.collider.gameObject.tag == "Player1" || hitTarget.collider.gameObject.tag == "Player2" || hitTarget.collider.gameObject.tag == "FusedPlayer")
-                {
-                    spotted = true;
-                    running = true;
-
-                    lastSeenPosition = hitTarget.point;
-
-                    bushPosition = Vector3.zero;
-                    target = hitTarget.collider.gameObject.transform;
-                }
-                else if (hitTarget.collider.gameObject.tag == "Bush" && spotted == true)
-                {
-
-                    bushPosition = hitTarget.collider.gameObject.transform.position;
-                }
-                else
-                {
-
-                    bushPosition = Vector3.zero;
-                }
-            }
-        
         }
-            */
-        /*}
-        else
-        {
-            running = false;
-        }*/
-       
+
 
         
 
@@ -307,34 +235,7 @@ public class EnemyScript : MonoBehaviour
         {
             transform.Translate(displacements * Time.deltaTime * speed, Space.Self);
         }
-        /*else
-        {
-            if (bushPosition == Vector3.zero)
-            {
-                if (transform.position != lastSeenPosition)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, lastSeenPosition, Time.deltaTime * speed);
-
-                    spotted = false;
-                }
-            }
-            else
-            {
-                if (transform.position != bushPosition && spotted == true)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, bushPosition, Time.deltaTime * speed);
-                    lookingStraight = true;
-
-                }
-                else if (transform.position == bushPosition)
-                {
-
-                    lookingStraight = false;
-                    spotted = false;
-                }
-            }
-        }
-        */
+        
         if (jumping == true)
         {
             transform.Translate(transform.up * jumpCurve.Evaluate(tJump) * power * Time.deltaTime, Space.World);
@@ -349,12 +250,6 @@ public class EnemyScript : MonoBehaviour
 
     void walk()
     {
-        // rotate towards the target
-        /* transform.forward = new Vector3(
-             transform.position.x, 
-             Vector3.RotateTowards(transform.forward, targetJourneyPoint.position - transform.position, rotationSpeed * Time.deltaTime, 0.0f).y, 
-             transform.position.z);
-             */
 
         transform.LookAt(new Vector3(targetJourneyPoint.position.x, transform.position.y, targetJourneyPoint.transform.position.z));
 
